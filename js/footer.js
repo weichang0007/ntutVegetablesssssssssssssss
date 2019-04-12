467f81fd556b5c52fabd0299b38ed3f942b5f9d4
smpy3468 <smpy3468@gmail.com>
2019-04-12 11:50:26 +0800

    將css分類 footer的css還沒寫好

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+
+footer = '<footer class="footer">\
+    <div class="footer__content">\
+    <p>Copyright</p>\
+    </div>\
+    <br>\
+    <div class="footer__content">\
+        <p>地址</p>\
+    </div>\
+    <br>\
+    <div class="footer__content">\
+        <p>電話</p>\
+    </div>\
+        </footer>';
+document.write(footer);
---
+++ js/nav.js
@@ -0,0 +1,22 @@
+nav = '<nav class="nav">\
+	<ul class="nav__list">\
+		<li class="nav__list-item">\
+			<a href="#" class="nav__list-item-link">主頁</a>\
+		</li>\
+		<li class="nav__list-item">\
+			<a href="#" class="nav__list-item-link">訂購頁面</a>\
+		</li>\
+		<li class="nav__list-item">\
+			<a href="#" class="nav__list-item-link">菜單</a>\
+			<ul class="nav__list--submenu">\
+				<li class="nav__list-item"><a href="#" class="nav__list-item-link--submenu">夏季菜單</a></li>\
+				<li class="nav__list-item"><a href="#" class="nav__list-item-link--submenu">冬季菜單</a></li>\
+				<li class="nav__list-item"><a href="#" class="nav__list-item-link--submenu">四季菜單</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav__list-item">\
+			<a href="#" class="nav__list-item-link">關於我們</a>\
+		</li>\
+	</ul>\
+    </nav>';
+document.write(nav);
