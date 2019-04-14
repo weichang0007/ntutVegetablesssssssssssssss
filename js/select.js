d359ffbe66668288bc62d4d0955674613d76af06
t294gji4wu6 <t294gji4wu6@gmail.com>
2019-04-14 17:13:16 +0800

    feedback & order html part

--- js/select.js
+++ js/select.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
     $("#btn1").click(function(){
       $("button").before("<div>"+$("#ss").html()+"</div>");
     });
+    $("#btn2").click(function(){
+      $("<div>"+$("#ss").html()+"</div>").remove();
+    });
   });
