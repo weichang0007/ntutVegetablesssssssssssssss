d14fd6409596f539aed924a01517a150686864ac
smpy3468 <smpy3468@gmail.com>
2019-04-15 23:52:00 +0800

    首頁下方新增測試頁數

--- js/index.js
+++ js/index.js
@@ -1,4 +1,45 @@
-var scoreCount = 0;
+var scoreCount = 15;//幾則評價
+var currentPage=1;//目前頁數
+var firstPage=1;//第一頁
+var lastPage = 5;//最後一頁
+
+$(document).ready(function(){
+        
+    $("#index__article__score__first-page").click(function()
+    {
+        currentPage = firstPage;
+        update();
+    });
+
+    $("#index__article__score__previous-page").click(function()
+    {
+        if(currentPage > 1)
+        {
+            currentPage--;
+        }
+        update();
+    });
+
+    $("#index__article__score__next-page").click(function()
+    {
+        if(currentPage < lastPage)
+        {
+            currentPage++;
+        }
+        update();
+    });
+
+    $("#index__article__score__last-page").click(function()
+    {
+        currentPage=lastPage;
+        update();
+    });    
+});
+
+function update()
+{
+    $("#index__article__score__current-page")[0].textContent = currentPage.toString();
+}
 
 function appendScoreElement()
 {
@@ -9,6 +50,9 @@ function appendScoreElement()
     score_block.appendChild(p);
 
     scoreCount++;
-    document.getElementById("index__article__score__count").textContent
-    ="共"+scoreCount.toString()+"筆評價";
+    document.getElementById("index__article__score-count").textContent
+    ="共"+scoreCount.toString()+"筆評價，第" + currentPage.toString() + "頁";
+
+
+    
 }
