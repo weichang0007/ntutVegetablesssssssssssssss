c3bce19f61c2a6798fd8ea993dc7200c5a474d4d
smpy3468 <smpy3468@gmail.com>
2019-04-16 15:25:03 +0800

    可顯示五筆評分 還不能換頁

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-var scoreCount = 15;//幾則評價
-var currentPage=1;//目前頁數
-var firstPage=1;//第一頁
-var lastPage = 5;//最後一頁
+var scoreCount = 0;//幾則評價
+var currentPage = 1;//目前頁數
+var firstPage = 1;//第一頁
+var lastPage = 1;//最後一頁
+var scoreArr=[];//評價陣列
 
 $(document).ready(function(){
         
@@ -39,20 +40,59 @@ $(document).ready(function(){
 function update()
 {
     $("#index__article__score__current-page")[0].textContent = currentPage.toString();
+    showScore();
 }
 
-function appendScoreElement()
+function getScore()//從網頁撈資料
 {
-    var p;
-    p=document.createElement("p");
-    p.textContent=scoreCount;
+    $.getJSON("https://script.googleusercontent.com/macros/echo?user_content_key=dXkY1PXnu9gf1G96WZWtylBqSMIWwTvgKxiEZa3_TDDPBVrv4AUz6PxZGNYVfmPUw_5spayZS7BCPGjkkU45kvO_qgbTezvKm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnI5daHpPPSKjQlPBWY9WuMUWHu9M7EOUWaSTcCMfOmyJy4P8aAIol9j21qbmr1esBycS1-3f5PS4&lib=MS_5mHCSVnuP__4TSP99mV2uMxfb3m1Vb",function(data)
+    {
+            var len=data.length;
+            for(var i = 0;i<len;i++)
+            {
+                var content="";
+                content += "id:" + data[i]['id'] + ", name:" + data[i]['name']
+                 + ", phone:" + data[i]['phone'] + ", email:" + data[i]['email']
+                 + ", dish:" + data[i]['dish'] + ", other:" + data[i]['other'];
+                 
+                scoreArr.push(content);//評分陣列增加一筆資料
+                scoreCount++;//評分數量增加
+                if(scoreCount <= 5)//一頁只顯示五筆
+                    appendScoreElement(content);
+                    
+                if((scoreCount - 1) % 5 == 0 && scoreCount > 5)//超過五筆增加頁面，每五筆增加一次
+                {
+                    lastPage++;
+                    document.getElementById("index__article__score__last-page").textContent="第"+lastPage.toString()+"頁";
+                }
+                document.getElementById("index__article__score-count").textContent
+                ="共"+scoreCount.toString()+"筆評價，第" + currentPage.toString() + "頁";
+                
+            }
+
+    });
+}
+
+function appendScoreElement(data)
+{
+    var p = document.createElement("p");
+    p.textContent = data;
     var score_block = document.getElementsByClassName("index__article__score")[0];
     score_block.appendChild(p);
+}
 
-    scoreCount++;
-    document.getElementById("index__article__score-count").textContent
-    ="共"+scoreCount.toString()+"筆評價，第" + currentPage.toString() + "頁";
-
-
-    
+function showScore()
+{
+    var score_block = document.getElementsByClassName("index__article__score")[0];
+    for(var i = 0;i<scoreCount;i++)
+    {
+        if(i < (currentPage - 1) * 5 || i >= currentPage * 5 - 1)
+        {
+            score_block[i].display = "none";
+        }
+        else
+        {
+            score_block[i].display = "inline";
+        }
+    }
 }
